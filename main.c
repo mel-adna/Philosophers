864da9ccf56f4668f16bca8620a66d38b2c94832
Mohamed El adnani <mel-adna@c2r2p9.1337.ma>
2025-04-30 12:26:13 +0100

    new files

--- main.c
+++ main.c
@@ -1,16 +1,23 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#include <stdlib.h>
 #include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-	int pid = fork();
+	int	x;
+	int	pid;
+
+	x = 2;
+	pid = fork();
 	if (pid == -1)
-		return -1;
-	printf("Process id %d\n", getpid());
+		return (-1);
+	if (pid == 0)
+		x++;
+	sleep(2);
+	printf("Value of x: %d\n", x);
 	if (pid != 0)
 		wait(NULL);
-	return 0;
+	return (0);
 }
--- philo.c
+++ philo.c
@@ -1,9 +1,18 @@
 #include "philo.h"
 
+int x =  2;
+
 void *routine(void *arg)
 {
+	x += 5;
     (void)arg;
-    printf("Process id %d\n", getpid());
+    printf("Value of x: %d\n", x);
+	return (NULL);
+}
+void *routine2(void *arg)
+{
+    (void)arg;
+    printf("Value of x: %d\n", x);
 	return (NULL);
 }
 
@@ -14,7 +23,7 @@ int	main(int argc, char **argv)
 	pthread_t t1, t2;
 	if (pthread_create(&t1, NULL, &routine, NULL) != 0)
 		return (1);
-	if (pthread_create(&t2, NULL, &routine, NULL) != 0)
+	if (pthread_create(&t2, NULL, &routine2, NULL) != 0)
 		return (2);
 	if (pthread_join(t1, NULL) != 0)
 		return (3);
