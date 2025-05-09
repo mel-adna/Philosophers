72c5c0bd66929da10643b2bd94e785066e841601
Mohamed El adnani <mohamedeladnani0@gmail.com>
2025-05-09 20:52:20 +0100

    push

--- parse_args.c
+++ parse_args.c
@@ -2,38 +2,77 @@
 
 int	ft_atoi(const char *str)
 {
-	int	res;
-	int	sign;
+	long long	res = 0;
+	int			sign = 1;
 
-	res = 0;
-	sign = 1;
 	while ((*str >= 9 && *str <= 13) || *str == ' ')
 		str++;
-	if (*str == '-' || *str == '+')
+	if (*str == '+' || *str == '-')
 	{
 		if (*str == '-')
-			sign *= -1;
+			sign = -1;
 		str++;
 	}
 	while (*str >= '0' && *str <= '9')
 	{
 		res = res * 10 + (*str - '0');
+		if ((sign == 1 && res > INT_MAX) || (sign == -1 && -res < INT_MIN))
+			return (-1);
 		str++;
 	}
-	return (res * sign);
+	return ((int)(res * sign));
 }
 
-int	parse_input(int argc, char **argv, t_data *data);
+static int	is_numeric(char *str)
 {
-    int i;
-    if (argc != 5 && argc != 6)
-        return (printf("Wrong number of arguments\n"));
-    i = 1;
-    while (i < argc)
-    {
-        if (is_numeric(argv[i]))
-            return (printf("Arguments must be numeric\n"));
-        i++;
-    }
-    return (0);
+	int i = 0;
+
+	if (!str || str[0] == '\0')
+		return (0);
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
 }
+
+static int	print_error(char *msg)
+{
+	printf("Error: %s\n", msg);
+	return (1);
+}
+
+int	parse_input(int argc, char **argv, t_data *data)
+{
+	int	i, value;
+
+	i = 1;
+	while (i < argc)
+	{
+		if (!is_numeric(argv[i]))
+			return (print_error("Arguments must be numeric"));
+		value = ft_atoi(argv[i]);
+		if (value == -1)
+			return (print_error("Argument too large or invalid"));
+		i++;
+	}
+	data->nbr_philos = ft_atoi(argv[1]);
+	data->time_to_die = ft_atoi(argv[2]);
+	data->time_to_eat = ft_atoi(argv[3]);
+	data->time_to_sleep = ft_atoi(argv[4]);
+	if (argc == 6)
+		data->meals_required = ft_atoi(argv[5]);
+	else
+		data->meals_required = -1;
+	if (data->nbr_philos < 1 || data->nbr_philos > 200
+		|| data->time_to_die <= 0 || data->time_to_eat <= 0
+		|| data->time_to_sleep <= 0 || (argc == 6 && data->meals_required <= 0))
+		return (print_error("Invalid argument values"));
+	return (data->someone_died = 0, 0);
+}
+
+
--- philo.c
+++ philo.c
@@ -4,6 +4,8 @@ int	main(int argc, char **argv)
 {
 	t_data	data;
 
+	if (argc != 5 && argc != 6)
+		return (printf("Wrong number of arguments"));
 	if (parse_input(argc, argv, &data))
-		return (printf("Invalid input!\n"));
+		return (1);
 }
